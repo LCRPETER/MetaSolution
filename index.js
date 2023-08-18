04ee93d128f398e25cf9f5370ecae0270e85c062
LCR Peter Simon <lcrpeter7@gmail.com>
2023-08-18 20:17:57 +0100

    version3

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const navbar = document.querySelector("nav");
-
+const btnLogin = document.getElementById("login-btn");
+const login = document.querySelector(".contenair-login");
+const signIn = document.querySelector(".contenair-sign-in");
+const register = document.querySelector(".register");
+const connect = document.querySelector(".register");
+const span = document.querySelector("span");
 let lastscroll = 0;
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 6) {
-    navbar.style.top = "-100px";
-  } else {
+  if (window.scrollY <= lastscroll) {
     navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-100px";
   }
   lastscroll = window.scrollY;
 });
+btnLogin.addEventListener("click", () => {
+  login.style.display = "block";
+});
+span.addEventListener("click", () => {
+  login.remove();
+});
+register.addEventListener("click", () => {
+  login.remove();
+  signIn.style.display = "block";
+});
+// connect.addEventListener("click", () => {
+//   login.style.display = "block";
+//   signIn.remove();
+// });
