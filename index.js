833a80de6f90400099126e23c75942478119df17
LCR Peter Simon <lcrpeter7@gmail.com>
2023-08-19 17:12:51 +0100

    MetaSolution4

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const navbar = document.querySelector("nav");
-const btnLogin = document.getElementById("login-btn");
+
 const login = document.querySelector(".contenair-login");
 const signIn = document.querySelector(".contenair-sign-in");
-const register = document.querySelector(".register");
-const connect = document.querySelector(".register");
-const span = document.querySelector("span");
+const Click = document.querySelectorAll(
+  "nav > ul > .btn-login, nav > div > form > p > span, nav > div > .login-remove"
+);
 let lastscroll = 0;
+let nb = 0;
 window.addEventListener("scroll", () => {
   if (window.scrollY <= lastscroll) {
     navbar.style.top = 0;
@@ -14,17 +15,29 @@ window.addEventListener("scroll", () => {
   }
   lastscroll = window.scrollY;
 });
-btnLogin.addEventListener("click", () => {
-  login.style.display = "block";
-});
-span.addEventListener("click", () => {
-  login.remove();
-});
-register.addEventListener("click", () => {
-  login.remove();
-  signIn.style.display = "block";
-});
-// connect.addEventListener("click", () => {
-//   login.style.display = "block";
-//   signIn.remove();
-// });
+for (let i = 0; i < Click.length; i++) {
+  Click[i].addEventListener("click", () => {
+    switch (Click[i]) {
+      case Click[nb]:
+        login.style.display = "block";
+        break;
+      case Click[nb + 2]:
+        login.style.display = "none";
+        signIn.style.display = "block";
+        break;
+      case Click[nb + 4]:
+        signIn.style.display = "none";
+        login.style.display = "block";
+        break;
+      case Click[nb + 1]:
+        login.style.display = "none";
+        break;
+      case Click[nb + 3]:
+        signIn.style.display = "none";
+        break;
+      default:
+        null;
+        break;
+    }
+  });
+}
